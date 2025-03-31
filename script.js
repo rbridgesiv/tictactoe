ab947ef946041df24eff37f06d51ddb006ab4cbb
Robert Bridges <robertbridges@Roberts-MacBook-Pro-2.local>
2025-03-31 20:52:07 +0300

    write GameController IIFE that controls player turns and switches; also add beginning script for checkWinner() function

--- script.js
+++ script.js
@@ -3,18 +3,43 @@ const Gameboard = (function () {
 
     const getBoard = () => board;
 
+    const checkWinner = () => {
+        
+    }
+
     const placeMarker = (index, marker) => {
-        if(board[index] === ""){
+        if (board[index] === ""){
             board[index] = marker;
             return true;
         }
         return false;
+        checkWinner();
     }
 
     return {getBoard, placeMarker};
 })();
 
-const Player (name, marker) {
+function Player (name, marker) {
     return {name, marker};
 }
 
+const GameController = (function () {
+
+    const player1 = Player("Alex", "X");
+    const player2 = Player("Zisha", "O");
+
+    let currentPlayer = player1;
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    function switchCurrentPlayer () {
+        if (currentPlayer === player1){
+            currentPlayer = player2;
+        }
+        else {
+            currentPlayer = player1;
+        }
+    }
+
+    return {getCurrentPlayer, switchCurrentPlayer};
+})();
